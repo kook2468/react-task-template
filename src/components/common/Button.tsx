a3cca35d28341e13145a0101dfc1f0d21b02e321
kook2468 <cdg9808@naver.com>
2025-05-18 16:16:23 +0900

    chore: 초기 기능 기반 폴더/구조 세팅

---
+++ src/components/common/Button.tsx
@@ -0,0 +1,13 @@
+import { ButtonHTMLAttributes } from "react";
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  children: React.ReactNode;
+}
+
+export default function Button({ children, ...props }: ButtonProps) {
+  return (
+    <button className="px-4 py-2 bg-blue-500 text-white rounded" {...props}>
+      {children}
+    </button>
+  );
+}
